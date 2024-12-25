1136718a6818dbb889cb21a5b859fff20c9a678b
YArrrrrJN <bonggoarjengreer@gmail.com>
2024-12-26 00:21:57 +0800

    History.java

--- History.java
+++ History.java
@@ -3,19 +3,21 @@ import java.util.ArrayList;
 public class History {
 
    public static class Record {
-      String type; 
-      String name;
-      String details; 
-   
-      public Record(String recordType, String recordName, String recordDetails) {
+      String type, name, details; 
+      double price;
+      
+      public Record(String recordType, String recordName, String recordDetails, double recordPrice) {
          type = recordType;
          name = recordName;
          details = recordDetails;
+         price = recordPrice;
       }
    
       public void recordDisplay() {
+         System.out.print("Updated History: \n");
          System.out.println("Type: " + type);
          System.out.println("Item: " + name);
+         System.out.println("Price: Php " + price);
          System.out.println("Details: " + details);
          System.out.println();
       }
@@ -27,26 +29,38 @@ public class History {
       historyRecord = new ArrayList<>();
    }
 
-   public void addSaleRecord(String name, int quantity, double totalAmount) {
+   public void addSaleRecord(String name, int quantity, double totalAmount, double price) {
       String details = "Sold " + quantity + " units. Total: Php " + totalAmount;
-      historyRecord.add(new Record("Sale", name, details));
+      historyRecord.add(new Record("Sale", name, details, price));
+   }
+   
+   public void priceChangeRecord(String name, double oldPrice, double newPrice){
+      String details = "Price changed from Php " + oldPrice + " to Php " + newPrice + ".\nNo items sold.";
+      historyRecord.add(new Record("Price Change", name, details, newPrice));
+      
    }
 
-   public void stockIncreaseRecord(String name, int quantityAdded) {
-      String details = "Stock added: " + name + " by " + quantityAdded + " units.\nNo items sold.";
-      historyRecord.add(new Record("Stock Added", name, details)   );
+   public void quantityChangeRecord(String name, int quantityChanged, double price) {
+      String details = "Quantity changed by " + quantityChanged + " units.";
+      historyRecord.add(new Record("Quantity Change", name, details, price));
+      
+   }
+
+   public void stockIncreaseRecord(String name, int quantityAdded, double price) {
+      String details = "Added " + quantityAdded + " units in the inventory.\nNo items sold.";
+      historyRecord.add(new Record("Stock Added", name, details, price));
    }
     
-   public void stockDecreaseRecord(String name, int quantityRemoved) {
-      String details = "Stock decreased by " + quantityRemoved + " units.";
-      historyRecord.add(new Record("Stock Decrease", name, details));
+   public void stockDecreaseRecord(String name, int quantityRemoved, double price) {
+      String details = "Stock decreased by " + quantityRemoved + " units.\nNo items sold.";
+      historyRecord.add(new Record("Stock Decrease", name, details, price));
    }
 
    public void displayHistory() {
       if (historyRecord.isEmpty()) {
          System.out.println("No history records available.");
       } else {
-         System.out.println("\nHISTORY (Stocks added/Items sold):\n");
+         System.out.println("\nSTORAGE HISTORY (Stocks added/decreased and Items sold):\n");
          for (Record record : historyRecord) {
             record.recordDisplay();
          }
